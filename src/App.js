dde21ac1c3d9a45b67f1036dfc9bd413a320a77a
Harish Singh <singhharish734@pec.edu>
2022-06-12 11:07:39 +0530

    props and state added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter></Counter>
+        <Counter by={1}/>
+        <Counter by={2}/>
+        <Counter by={5}/>
+        <Counter by={10}/>
+        <Counter by={50}/>
+        <Counter by={100}/>
       </header>
     </div>
   );
--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -2,22 +2,34 @@ import { Component } from "react";
 import "./counter.css"
 
 class Counter extends Component {
+
+    // initial state is stored in constructor
+    constructor(){
+        super(); // Never Miss
+        this.state = {
+            counter: 0
+        }
+        this.increment = this.increment.bind(this);
+        // bind 'this' with increment() method in order to use 'this' inside increment() method
+
+        // we can use arrow function if you dont want to bind each methods
+    }
+
+    // Heart and soul, render() method
     render() {
         return (
             <div className="counter">
-                <button name="button" onClick={this.increment}>+1</button>
-                <button name="button">+2</button>
-                <button name="button">+5</button>
-                <button name="button">+10</button>
-                <button name="button">+50</button>
-                <button name="button">+100</button>
-                <h1 className="count">0</h1>
+                <button name="button" onClick={this.increment}>+{this.props.by}</button>
+                <h1 className="count">{this.state.counter}</h1>
             </div>
         )
     }
 
     increment(){
-        console.log("incremented")
+        // use setState instead of directly updating state
+        this.setState(
+            {counter: this.state.counter+=this.props.by}
+        );
     }
 }
 
